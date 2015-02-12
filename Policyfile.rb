cf0885f3f2f5edaa44bf8d5e5de4c4d0efa51411
danielsdeleo <dan@getchef.com>
2015-02-12 09:15:57 -0800

    updates for native mode

--- Policyfile.rb
+++ Policyfile.rb
@@ -4,4 +4,6 @@ default_source :community
 
 run_list "apt", "java", "jenkins::master", "recipe[policyfile_demo]"
 
+named_run_list :update_jenkins, "jenkins::master", "recipe[policyfile_demo]"
+
 cookbook "policyfile_demo", path: "cookbooks/policyfile_demo"
--- etc/export-zero.rb
+++ etc/export-zero.rb
@@ -1,6 +1,7 @@
-
+policy_document_native_api true
 versioned_cookbooks true
 
-chef_server_url "http://localhost:8889"
+#chef_server_url "http://localhost:8889/organizations/demo-org"
+chef_server_url "http://localhost:8889/"
 client_key File.expand_path("../user.pem", __FILE__)
 node_name "workstation"
